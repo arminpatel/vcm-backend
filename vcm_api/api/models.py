a0d9c8ff7d6728f2b4324eab48d52b352a5ecb73
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-04-04 01:21:05 +0530

    add contest and problem model

--- vcm_api/api/models.py
+++ vcm_api/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Problem(models.Model):
+    id = models.CharField(max_length=20, blank=False, primary_key=True)
+    name = models.CharField(max_length=128, blank=False)
+    link = models.URLField(max_length=256, blank=False)
+
+class Contest(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    start_date_time = models.DateTimeField()
+    duration = models.DurationField()
+    problems = models.ManyToManyField('Problem')
+
+
