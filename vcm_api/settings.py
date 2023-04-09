4a58d9fd38dbdf6ab96b6cd836e613c44d442bc2
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-04-09 10:24:18 +0530

    setup linting and lint workflow

--- vcm_api/settings.py
+++ vcm_api/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
