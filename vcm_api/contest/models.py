77c31249ec539bad9317d68babb63832d7b8ffbf
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-05-11 09:11:28 +0530

    setup cors and fix linting errors (#23)

--- vcm_api/contest/models.py
+++ vcm_api/contest/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class Contest(models.Model):
--- vcm_api/settings.py
+++ vcm_api/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "vcm_api.contest"
-    
+    "vcm_api.contest",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "vcm_api.urls"
@@ -122,3 +123,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  "http://localhost:8000",
+  "http://localhost:3000",
+)
