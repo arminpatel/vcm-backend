af43996d1127865a7f5521dedcad416cdc6733b6
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-05-16 18:49:42 +0530

    refactor and remove unecessary code

--- vcm_api/problem/models.py
+++ vcm_api/problem/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class Problem(models.Model):
-    id = models.CharField(max_length=40, primary_key=True, blank=False, null=False)
-    name = models.CharField(max_length=40, blank=False, null=False)
-    link = models.URLField(max_length=1000, blank=False, null=False)
+    id = models.CharField(max_length=40, primary_key=True)
+    name = models.CharField(max_length=40)
+    link = models.URLField(max_length=1000)
     solved_by = models.ManyToManyField(get_user_model(), related_name="problems_solved")
