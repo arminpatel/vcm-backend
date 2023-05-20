ff7880971abab3f7c54d9d6bdbeb7acdf586e197
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-05-20 11:29:02 +0530

    fix linting errors

--- vcm_api/test_token.py
+++ vcm_api/test_token.py
@@ -1,11 +1,7 @@
-from datetime import timedelta
 import pytest
-import time
-
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APIClient
-from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
--- vcm_api/urls.py
+++ vcm_api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView, 
     TokenRefreshView,
+    TokenObtainPairView,
     TokenVerifyView
 )
 
