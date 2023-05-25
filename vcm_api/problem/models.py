aefdae7c236dcf57f58be6ddc5aa8616d1c3a3fd
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-05-25 11:26:03 +0530

    remove solved_by in problem model

--- vcm_api/problem/models.py
+++ vcm_api/problem/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class Problem(models.Model):
     id = models.CharField(max_length=40, primary_key=True)
     name = models.CharField(max_length=40)
     link = models.URLField(max_length=1000)
-    solved_by = models.ManyToManyField(get_user_model(), related_name="problems_solved")
