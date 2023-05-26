dfd7fda5bf6f66662eeefc6eb942389aee5842de
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-05-26 12:19:16 +0530

    remove solved_by in problem model (#46)

--- vcm_api/submission/models.py
+++ vcm_api/submission/models.py
@@ -5,8 +5,8 @@ from vcm_api.problem.models import Problem
 
 
 class Submission(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="submissions")
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name="submissions")
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE, related_name="submissions")
     time = models.DateTimeField()
     correct_answer = models.BooleanField()
--- vcm_api/submission/test_submission.py
+++ vcm_api/submission/test_submission.py
@@ -33,9 +33,32 @@ def test_submission_creation_successful():
                                                 time=datetime(2022, 11, 23, 19, 10, 15, 27,
                                                               tzinfo=pytz.UTC), correct_answer=True)
 
+    td = test_submission.time - test_submission.contest.start_date_time
+
     assert Submission.objects.count() == 1
     assert test_submission.contest.name == "Good To Go"
 
-    td = test_submission.time - test_submission.contest.start_date_time
-    print(td)
     assert td.days == 0 and td.seconds == 903 and td.microseconds == 4
+
+
+@pytest.mark.django_db
+def testSubmission_problemAndUserLinkedProperly():
+    test_problem = Problem.objects.create(
+        id="CF1811F", name="Is it Flower?", link="https://codeforces.com/problemset/problem/1811/F")
+
+    test_contest = Contest.objects.create(name="Good To Go", start_date_time=datetime(
+        2022, 11, 23, 18, 55, 12, 23, tzinfo=pytz.UTC), duration=timedelta(hours=2, minutes=30))
+
+    test_user = User.objects.create(
+        first_name="test", last_name="user-1", username="theoden42", password="test_me123")
+
+    Submission.objects.create(user=test_user,
+                              contest=test_contest,
+                              problem=test_problem,
+                              time=datetime(2022, 11, 23, 19, 10, 15, 27,
+                                            tzinfo=pytz.UTC), correct_answer=True)
+
+    assert Submission.objects.count() == 1
+    assert test_problem.submissions.count() == 1
+    assert test_user.submissions.count() == 1
+    assert test_user.submissions.first().problem == test_problem
