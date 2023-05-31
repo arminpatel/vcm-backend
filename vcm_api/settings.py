8371a62d2c3e7335854666ab435a8f2c71294ced
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-05-31 17:35:20 +0530

    update allowed hosts

--- vcm_api/settings.py
+++ vcm_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arminpatel.pythoneverywhere.com', '127.0.0.1']
 
 # Application definition
 
