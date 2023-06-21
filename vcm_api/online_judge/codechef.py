6a5676ff7f0503644297397bd2a209f96fab27e8
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-06-22 00:47:59 +0530

    add wrapper for codechef

---
+++ vcm_api/online_judge/codechef.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import datetime
+import pytz
+
+class Codechef:
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1200")
+        self.driver = webdriver.Chrome(options=options)
+
+    def check_solved(self, user, problem_code, contest_start_time, duration):
+        # Assuming constest_start_time and duration is in seconds
+        """
+        Check if a user has solved a problem in a contest
+        """
+
+        url = f"https://www.codechef.com/status/{problem_code}?usernames={user}"
+
+        page_source = self._get_page_source(url)
+
+        soup = BeautifulSoup(page_source, "html.parser")
+
+        rows = soup.find("tbody").find_all("tr")
+
+        for row in rows:
+            submission_time = row.find_all("td")[1].span['title']
+            submission_time = self._get_time(submission_time)
+
+            is_accepted = row.find_all("td")[3].div.div['title'] == "accepted"
+
+            if is_accepted and  \
+               contest_start_time <= submission_time <= contest_start_time + duration:
+                return True
+
+        return False
+
+    def _get_page_source(self, url):
+        self.driver.get(url)
+        sleep(1)
+        return self.driver.page_source
+
+    def _get_time(self, time):
+        """
+        Convert time obtained from codechef to unix timestamp
+        """
+        time = time.split('(')[0].strip()
+        print(time)
+        aware_time = datetime.datetime.strptime(time, "%a %b %d %Y %H:%M:%S %Z%z")
+
+        return aware_time.timestamp()
+
+
+if __name__ == "__main__":
+    print(Codechef().check_solved("sanu_sona", "LASTRBS", 1687321800, 0.5 * 3600))
---
+++ vcm_api/online_judge/test_codechef.py
@@ -0,0 +1,11 @@
+import pytest
+from codechef import Codechef
+
+def test_check_solved():
+    codechef = Codechef()
+
+    # A user successfully submitted a solution to the problem at Wed Jun 21 2023 10:36:22 (IST)
+    # we would test that
+
+    assert not codechef.check_solved('sanu_sona', "LASTRBS",  1687321800, 0.5 * 3600)
+    assert codechef.check_solved('sanu_sona', "LASTRBS",  1687321800, 2 * 3600)
