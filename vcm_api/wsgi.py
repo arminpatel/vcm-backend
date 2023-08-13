e427e6a430ff1d817e35d6308e4ffbde81963b1c
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-13 20:35:03 +0530

    fix wsgi.py (#91)

--- vcm_api/wsgi.py
+++ vcm_api/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcm_api.settings.production")
 
 application = get_wsgi_application()
