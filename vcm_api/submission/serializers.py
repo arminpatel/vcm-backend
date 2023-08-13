97a8a54e2d971d9a5ff41e62d67a32fea65e9bc1
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-08-13 19:44:52 +0530

    add tests for submission creation and fix minor bugs (#85)
    * add test for submission creation and fix minor bugs
    
    * fix test name and add test to check admin permission for submission view

--- vcm_api/submission/serializers.py
+++ vcm_api/submission/serializers.py
@@ -4,6 +4,7 @@ from vcm_api.problem.models import Problem
 from django.contrib.auth import get_user_model
 from vcm_api.online_judge import codechef, codeforces, atcoder
 from datetime import datetime
+import pytz
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
@@ -16,13 +17,13 @@ class SubmissionSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         current_user = self.context['request'].user
         if self.context['request'].data['problem_id'] is None:
-            raise serializers.ValidationError(detail="Problem ID field is required")
+            raise serializers.ValidationError(detail="problem id field is required")
 
         problem_id = self.context['request'].data['problem_id']
 
         required_problem = Problem.objects.filter(id=problem_id).first()
         if required_problem is None:
-            raise serializers.ValidationError(detail="Problem ID is incorrect")
+            raise serializers.ValidationError(detail="problem id is incorrect")
         required_online_judge = required_problem.online_judge
         required_contest = required_problem.contest
 
@@ -31,7 +32,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
         if required_online_judge == "codeforces":
             codeforces_username = current_user.profile.cf_handle
             if codeforces_username is None:
-                raise serializers.ValidationError(detial="Codeforces Handle is Not Available")
+                raise serializers.ValidationError(detail="codeforces handle is not available")
 
             required_problem_id = codeforces.get_problem_id(
                 codeforces.convert_problem_url(required_problem.link))
@@ -46,7 +47,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
         elif required_online_judge == "codechef":
             codechef_username = current_user.profile.cc_handle
             if codechef_username is None:
-                raise serializers.ValidationError(detail="Codechef Handle is Not Available")
+                raise serializers.ValidationError(detail="codechef handle is not available")
 
             instance_codechef = codechef.Codechef()
             required_problem_id = instance_codechef.get_problem_id(required_problem.link)
@@ -61,7 +62,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
         else:
             atcoder_username = current_user.profile.ac_handle
             if atcoder_username is None:
-                raise serializers.ValidationError(detail="Atcoder Handle is Not Available")
+                raise serializers.ValidationError(detail="atcoder handle is not available")
             required_problem_id = atcoder.get_problem_id(required_problem.link)
             required_contest_starttime = datetime.timestamp(required_contest.start_date_time)
             required_contest_duration = required_contest.duration.total_seconds()
@@ -72,17 +73,17 @@ class SubmissionSerializer(serializers.ModelSerializer):
                 int(required_contest_duration))
 
         if solved_status is False:
-            raise serializers.ValidationError(detail="Problem has not been solved")
+            raise serializers.ValidationError(detail="problem is not solved")
 
         existing_submission = Submission.objects.filter(
             problem=required_problem, correct_answer=True).first()
 
         if existing_submission is not None:
-            raise serializers.ValidationError(detail="Problem is already solved by the user")
+            raise serializers.ValidationError(detail="problem is already solved")
 
         new_submission = Submission.objects.create(
             user=current_user,
             problem=required_problem,
-            time=datetime.now(),
+            time=datetime.now(pytz.UTC),
             correct_answer=True)
         return new_submission
