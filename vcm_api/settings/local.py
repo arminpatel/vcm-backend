ba9b8449bbf97d3b56321368dc5aa460303d27a2
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-21 23:17:01 +0530

    add start_prod

--- vcm_api/settings/local.py
+++ vcm_api/settings/local.py
@@ -12,6 +12,6 @@ CORS_ORIGIN_WHITELIST = (
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3", # noqa: F405
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
     }
 }
