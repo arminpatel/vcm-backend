254dd0805c8aaf6fca537f0a604eb277b0032556
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-21 18:38:43 +0530

    fix start file

--- start_prod.sh
+++ start_prod.sh
@@ -5,4 +5,4 @@ python manage.py makemigrations problem &&
 python manage.py makemigrations submission &&
 python manage.py makemigrations &&
 python manage.py migrate &&
-gunicorn --bind="0.0.0.0:5544" vcm_api.wsgi:application
+gunicorn --bind="0.0.0.0" vcm_api.wsgi:application
