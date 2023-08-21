adbbe3242d1818fa45e5eae59a0b26f505165eec
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-21 18:18:30 +0530

    add start file for production

---
+++ start_prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python manage.py makemigrations user &&
+python manage.py makemigrations contest &&
+python manage.py makemigrations problem &&
+python manage.py makemigrations submission &&
+python manage.py makemigrations &&
+python manage.py migrate &&
+gunicorn --bind=0.0.0.0:5544 vcm_api.wsgi:application
