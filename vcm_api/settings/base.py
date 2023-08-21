6defb2830629dd108ac6a13c321c47347b5a3858
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-21 23:02:28 +0530

    add psycopg and fix staticfiles storage (#97)

--- vcm_api/settings/base.py
+++ vcm_api/settings/base.py
@@ -114,8 +114,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whiteenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
