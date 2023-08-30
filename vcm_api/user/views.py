aa5c2a56727f3b344ddedc4a08eaa59ed72f491f
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-30 13:30:09 +0530

    fix lint errors

--- vcm_api/user/views.py
+++ vcm_api/user/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 
 from vcm_api.user.serializers import UserSerializer
