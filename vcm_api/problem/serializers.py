246487a08c0c994b3ce5ffb04fe8847cf4e46440
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-09-30 16:43:43 +0530

    Add Docker Compose setup for deployment and a minor refactoring change (#111)
    * refactoring changes
    
    * add docker compose set up for deployment

--- vcm_api/problem/serializers.py
+++ vcm_api/problem/serializers.py
@@ -4,16 +4,17 @@ from vcm_api.submission.models import Submission
 
 
 class ProblemSerializer(serializers.ModelSerializer):
+    is_solved = serializers.SerializerMethodField()
+
     class Meta:
         model = Problem
         fields = ['id', 'name', 'link', 'score', 'online_judge', 'is_solved']
-
-    is_solved = serializers.SerializerMethodField()
 
     def get_is_solved(self, instance):
         current_user = self.context['request'].user
         if current_user.is_anonymous:
             return False
+
         firstsub = Submission.objects.filter(user=current_user, problem=instance).first()
         if firstsub is None:
             return False
